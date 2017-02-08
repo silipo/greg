48622964c8f5c882c92f34191e46ad0f69c359c5
Marco <silipo@istat.it>
2017-02-08 23:47:38 +0100

    if key is null it is no longer serialized

--- src/main/java/it/istat/dcit/itc/greg/dto/RuleDTO.java
+++ src/main/java/it/istat/dcit/itc/greg/dto/RuleDTO.java
@@ -1,5 +1,9 @@
 package it.istat.dcit.itc.greg.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_NULL)
 public class RuleDTO {
 
     private String key;
